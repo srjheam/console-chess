d0f14d065a9dc6e7a6705702dfa221d6405cefa8
Jheam Storch Ross <58143641+srjheam@users.noreply.github.com>
2022-01-17 21:37:24 -0300

    Add try-catch block in Main to log unexpected exceptions

--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -1,13 +1,35 @@
 ﻿using Chess;
 
+using System;
+using System.IO;
+
 namespace ConsoleChess
 {
     class Program
     {
         static void Main()
         {
-            var chessGame = new ChessMatch();
-            chessGame.Start();
+            try
+            {
+                var chessGame = new ChessMatch();
+                chessGame.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.Clear();
+                Console.WriteLine("Oops! Looks like someone has checkmate the program. This error will be logged. Sorry, but your game had to be stopped.");
+
+                try
+                {
+                    var now = DateTime.UtcNow;
+                    using var sw = new StreamWriter(Path.Combine(Path.GetTempPath(), $"consolechess-{now.Ticks}.log"), true);
+                    sw.WriteLine($"{now:O} - consolechess - CRITICAL - {ex.ToString().Replace(Environment.NewLine, " ")}");
+                }
+                catch (Exception) { }
+
+                Console.ReadKey(true);
+            }
         }
     }
 }
